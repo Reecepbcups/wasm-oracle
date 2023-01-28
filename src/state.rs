a54b0a97ed6eed668d673c18b42b3f73bbe47e1f
Reece Williams <reecepbcups@gmail.com>
2023-01-28 16:56:34 -0600

    TWAP comment

--- src/state.rs
+++ src/state.rs
@@ -7,11 +7,17 @@ pub const INFORMATION: Item<ContractInformationResponse> = Item::new("info");
 
 pub const ALLOWED_DATA: Map<&str, bool> = Map::new("allowed");
 
-// Address, lastBlockSubmitedFor
+// Address, lastBlockSubmittedFor
 pub const ADDRESSES: Map<&str, u64> = Map::new("addresses");
 
 // Ex: (denom, address): price
 pub const VALUES: Map<(&str, &str), u64> = Map::new("values");
+
+// TWAP = on submit, every X blocks save the value here. We don't need to save every block.
+// Then rotate it out like how I did my marketplace.
+// ID: vec<(block, value)> max of XXX long, set every XXXX blocks 
+// Ex: every 100 blocks, save the average price here. Keep the last 100 instances of this data = .
+// Have a function return the TWAP average
 
 // get all values for a id by the first key in the tuple
 pub fn get_values(deps: Deps, denom: &str) -> Vec<u64> {
